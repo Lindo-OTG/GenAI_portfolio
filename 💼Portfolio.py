ac35d3088eb06b295d5f3b498f5eff1f6aa51291
Lindo-OTG <lindo03.dlomo@gmail>
2024-05-10 14:23:08 +0200

    fix project images

--- 💼Portfolio.py
+++ 💼Portfolio.py
@@ -103,11 +103,7 @@ with st.container():
             #     f'<a href="{project["link"]}" target="_blank" class="portfolio-item" data-id="3"><img src="{project["image_url"]}" alt="{project["title"]}" style="width:100%;height:auto;" /></a>',
             #     unsafe_allow_html=True,
             # )
-            st.markdown(
-                f'<a href="{project["link"]}" target="_blank" class="portfolio-item" data-id="3">{st.image(Image.open({project["image_url"]}))}</a>',
-                unsafe_allow_html=True,
-            )
-            # st.image(Image.open({project["image_url"]}))
+            st.image(Image.open({project["image_url"]}))
             st.markdown(f'<p style="font-size: 16px; font-weight: bold;">{project["title"]}</p>', unsafe_allow_html=True)
             st.markdown(f'<p style="font-size: 14px">{project["description"]}</p>', unsafe_allow_html=True)
     col1, col2, col3 = st.columns([1,1,1])
